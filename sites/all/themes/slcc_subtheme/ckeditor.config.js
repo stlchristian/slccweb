b15dec43a230dab98fdd6d8414b05486de727e25
Elliot Voris <elliotfriend@gmail.com>
2014-07-01 16:11:00 -0500

    Issue #34: Installed and configured CKEditor module
    The contrib module works much better, and looks A LOT nicer. I am
    also able to export the configurations into a feature, which is a
    huge bonus!
    
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

---
+++ sites/all/themes/slcc_subtheme/ckeditor.config.js
@@ -0,0 +1,13 @@
+CKEDITOR.editorConfig = function(config) {
+    // Allow <i class="xyz"></i> for Font Awesome.
+    config.protectedSource.push( /<i class=\".*\"\><\/i\>/g );
+
+    // Allow <div class="xyz"></div> tags through.
+    config.protectedSource.push( /<div class=\".*\"\>.*?<\/div\>/g );
+
+    // Activate automatic AFC mode (if not already set).
+    config.allowedContent = true;
+};
+
+CKEDITOR.dtd.$removeEmpty['i'] = false;
+CKEDITOR.dtd.$inline = i;
