843a0b63e1396162aa9d6ae82ab80191888b50ed
Elliot Voris <elliotfriend@gmail.com>
2014-07-17 14:48:29 -0500

    I needed to adjust the path to the redis module in settings.php
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -4,13 +4,13 @@
 if (defined('PANTHEON_ENVIRONMENT')) {
   // Use Redis for caching.
   $conf['redis_client_interface'] = 'PhpRedis';
-  $conf['cache_backends'][] = 'sites/all/modules/redis/redis.autoload.inc';
+  $conf['cache_backends'][] = 'sites/all/modules/contrib/redis/redis.autoload.inc';
   $conf['cache_default_class'] = 'Redis_Cache';
   $conf['cache_prefix'] = array('default' => 'pantheon-redis');
   // Do not use Redis for cache_form (no performance difference).
   $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
   // Use Redis for Drupal locks (semaphore).
-  $conf['lock_inc'] = 'sites/all/modules/redis/redis.lock.inc';
+  $conf['lock_inc'] = 'sites/all/modules/contrib/redis/redis.lock.inc';
 }
 /**
  * @file
