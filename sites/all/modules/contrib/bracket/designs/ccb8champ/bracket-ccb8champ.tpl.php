06dcec1d3560f27ab3f78308ca3b3f7facdcd403
Elliot Voris <elliotfriend@gmail.com>
2014-10-13 14:18:32 -0500

    Updated the bracket module to output properly using HTML
    I like the HTML version of the output better. For some reason, I
    didn't want to just create an image out of the bracket. I like
    having HTML in there now.
    
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

--- sites/all/modules/contrib/bracket/designs/ccb8champ/bracket-ccb8champ.tpl.php
+++ sites/all/modules/contrib/bracket/designs/ccb8champ/bracket-ccb8champ.tpl.php
@@ -17,6 +17,23 @@ drupal_add_css(drupal_get_path('module', 'bracket') . '/designs/ccb8champ/bracke
   <?php if ($node->comments) { ?>
   <div class="bracket-comments"><?php print nl2br(filter_xss_admin($node->comments)) ?></div>
   <?php } ?>
+  <div id="lower-results" class="bracket-results bracket-results-right">
+    <div id="fifth-results-spacer"></div>
+    <?php print theme('bracket_result', array('node' => $node, 'result_index' => 5, 'orientation' => 'right')) ?>
+  </div>
+  <div id="round-5" class="bracket-round">
+    <div class="bracket-round-comment"><?php print $node->round[5]->comment ? filter_xss_admin($node->round[5]->comment) : '&nbsp;' ?></div>
+    <div id="round-2-loser-round-spacer"></div>
+    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 5, 'match_index' => 1, 'orientation' => 'right')) ?>
+  </div>
+  <div id="round-4" class="bracket-round">
+    <div class="bracket-round-comment"><?php print $node->round[4]->comment ? filter_xss_admin($node->round[4]->comment) : '&nbsp;' ?></div>
+    <div id="round-4-spacer"></div>
+    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 4, 'match_index' => 1, 'orientation' => 'right')) ?>
+    <div class="bracket-round-4-match-spacer"></div>
+    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 4, 'match_index' => 2, 'orientation' => 'right')) ?>
+    <div class="bracket-footer"><?php print nl2br(filter_xss_admin($node->footer)) ?></div>
+  </div>
   <div id="round-1" class="bracket-round">
     <div class="bracket-round-comment"><?php print $node->round[1]->comment ? filter_xss_admin($node->round[1]->comment) : '&nbsp;' ?></div>
     <div id="round-1-spacer"></div>
@@ -28,10 +45,6 @@ drupal_add_css(drupal_get_path('module', 'bracket') . '/designs/ccb8champ/bracke
     <div class="bracket-round-1-match-spacer"></div>
     <?php print theme('bracket_match', array('node' => $node, 'round_index' => 1, 'match_index' => 4, 'orientation' => 'left')) ?>
     <div id="round-1-loser-round-spacer"></div>
-    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 4, 'match_index' => 1, 'orientation' => 'left')) ?>
-    <div class="bracket-round-4-match-spacer"></div>
-    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 4, 'match_index' => 2, 'orientation' => 'left')) ?>
-    <div class="bracket-footer"><?php print nl2br(filter_xss_admin($node->footer)) ?></div>
   </div>
   <div id="round-2" class="bracket-round">
     <div class="bracket-round-comment"><?php print $node->round[2]->comment ? filter_xss_admin($node->round[2]->comment) : '&nbsp;' ?></div>
@@ -39,10 +52,6 @@ drupal_add_css(drupal_get_path('module', 'bracket') . '/designs/ccb8champ/bracke
     <?php print theme('bracket_match', array('node' => $node, 'round_index' => 2, 'match_index' => 1, 'orientation' => 'left')) ?>
     <div class="bracket-round-2-match-spacer"></div>
     <?php print theme('bracket_match', array('node' => $node, 'round_index' => 2, 'match_index' => 2, 'orientation' => 'left')) ?>
-    <div id="round-2-loser-round-spacer"></div>
-    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 5, 'match_index' => 1, 'orientation' => 'left')) ?>
-    <div class="bracket-round-5-match-spacer"></div>
-    <?php print theme('bracket_match', array('node' => $node, 'round_index' => 5, 'match_index' => 2, 'orientation' => 'left')) ?>
   </div>
   <div id="round-3" class="bracket-round">
     <div class="bracket-round-comment"><?php print $node->round[3]->comment ? filter_xss_admin($node->round[3]->comment) : '&nbsp;' ?></div>
@@ -58,8 +67,6 @@ drupal_add_css(drupal_get_path('module', 'bracket') . '/designs/ccb8champ/bracke
     <?php print theme('bracket_result', array('node' => $node, 'result_index' => 2, 'orientation' => 'left')) ?>
     <div id="loser-results-spacer"></div>
     <?php print theme('bracket_result', array('node' => $node, 'result_index' => 3, 'orientation' => 'left')) ?>
-    <div class="bracket-result-spacer"></div>
-    <?php print theme('bracket_result', array('node' => $node, 'result_index' => 4, 'orientation' => 'left')) ?>
   </div>
   <?php print theme('bracket_images', array('node' => $node)) ?>
 </div>
