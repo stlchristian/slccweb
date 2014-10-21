75bda3d6d9a6440be0757aa5f199ca90af5bdbb5
Elliot Voris <elliotfriend@gmail.com>
2014-10-21 13:46:35 -0500

    Issue #47: Commenting out score/comment display in the bracket
    Since we're working on creating our own page to display the game
    recaps, the score and the comments (goal scorers) will be displayed
    on that page. We don't need them here, and they make the bracket
    look quite a bit cluttered, to me.
    
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

--- sites/all/modules/contrib/bracket/templates/bracket-match.tpl.php
+++ sites/all/modules/contrib/bracket/templates/bracket-match.tpl.php
@@ -14,13 +14,15 @@
 <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>" class="bracket-match round-<?php print $round_index ?>-match-block">
   <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1-block" class="bracket-match-competitor-block">
     <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1" class="bracket-match-competitor bracket-match-competitor-<?php print $orientation ?><?php if ($node->round[$round_index]->match[$match_index]->win[1]) print ' bracket-match-winner' ?>"><?php print $node->round[$round_index]->match[$match_index]->cname[1] ? filter_xss_admin($node->round[$round_index]->match[$match_index]->cname[1]) : '&nbsp;' ?></div>
+<!--
     <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1-score" class="bracket-match-score bracket-match-score-<?php print $orientation ?>"><?php print filter_xss_admin($node->round[$round_index]->match[$match_index]->score[1]) ?></div>
+-->
   </div>
   <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-block" class="bracket-match-block bracket-match-block-<?php print $orientation ?> round-<?php print $round_index ?>-match">
-    <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1-comment-block" class="bracket-match-competitor-comment-block">
+<!--    <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1-comment-block" class="bracket-match-competitor-comment-block">
       <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1-comment" class="bracket-match-competitor-comment bracket-match-competitor-comment-<?php print $orientation ?>"><?php print $node->round[$round_index]->match[$match_index]->comp_comment[1] ? filter_xss_admin($node->round[$round_index]->match[$match_index]->comp_comment[1]) : '&nbsp;' ?></div>                
       <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-1-home" class="bracket-match-home bracket-match-home-<?php print $orientation ?>"><?php print $node->round[$round_index]->match[$match_index]->home[1] ? 'H' : '&nbsp;' ?></div>
-    </div>
+    </div>-->
     <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-comment-block" class="bracket-match-comment-block round-<?php print $round_index ?>-match-comment-block">
       <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-comment-1" class="bracket-match-comment bracket-match-comment-<?php print $orientation ?>"><?php print filter_xss_admin($node->round[$round_index]->match[$match_index]->comment[1]) ?></div>
       <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-comment-2" class="bracket-match-comment bracket-match-comment-<?php print $orientation ?>"><?php print filter_xss_admin($node->round[$round_index]->match[$match_index]->comment[2]) ?></div>
@@ -30,11 +32,13 @@
     <?php } ?>
     <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2-block" class="bracket-match-competitor-block bracket-match-bottom-competitor-block">
       <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2" class="bracket-match-competitor bracket-match-competitor-<?php print $orientation ?><?php if ($node->round[$round_index]->match[$match_index]->win[2]) print " bracket-match-winner" ?>"><?php print $node->round[$round_index]->match[$match_index]->cname[2] ? filter_xss_admin($node->round[$round_index]->match[$match_index]->cname[2]) : '&nbsp;' ?></div>
+<!--
       <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2-score" class="bracket-match-score bracket-match-score-<?php print $orientation ?>"><?php print filter_xss_admin($node->round[$round_index]->match[$match_index]->score[2]) ?></div>
+-->
     </div>
   </div>
-  <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2-comment-block" class="bracket-match-competitor-comment-block">
+<!--  <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2-comment-block" class="bracket-match-competitor-comment-block">
     <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2-score" class="bracket-match-competitor-comment bracket-match-competitor-comment-<?php print $orientation ?>"><?php print $node->round[$round_index]->match[$match_index]->comp_comment[2] ? filter_xss_admin($node->round[$round_index]->match[$match_index]->comp_comment[2]) : '&nbsp;' ?></div>                
     <div id="round-<?php print $round_index ?>-match-<?php print $match_index ?>-competitor-2-home" class="bracket-match-home bracket-match-home-<?php print $orientation ?>"><?php print $node->round[$round_index]->match[$match_index]->home[2] ? 'H' : '&nbsp;' ?></div>
-  </div>
+  </div>-->
 </div>
