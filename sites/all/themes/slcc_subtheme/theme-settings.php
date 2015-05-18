5cb006b32d698b55690379b08101cf788fa20803
Elliot J. Voris <elliot@voris.me>
2015-05-18 14:20:47 -0500

    Issue #55: Stupid line that I forgot to delete...
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- sites/all/themes/slcc_subtheme/theme-settings.php
+++ sites/all/themes/slcc_subtheme/theme-settings.php
@@ -4,7 +4,6 @@
  * Implements hook_form_system_theme_settings_alter().
  */
 function slcc_subtheme_form_system_theme_settings_alter(&$form, $form_state) {
-
   $form['nucleus']['global_settings']['favicon']['settings']['favicon_path']['#default_value'] = '';
   $form['nucleus']['global_settings']['favicon']['default_favicon']['#default_value'] = 0;
 }
